a37108f79e0241d7a80614c702effa7ad2678521
Denis Huber <dhuber@ltutech.com>
2022-03-21 17:50:42 +0100

    Add field is_on_shift on models bus/driver

--- padam_django/apps/fleet/models/buses.py
+++ padam_django/apps/fleet/models/buses.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Bus(models.Model):
     licence_plate = models.CharField("Name of the bus", max_length=10)
+    is_on_shift = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = "Buses"
--- padam_django/apps/fleet/models/drivers.py
+++ padam_django/apps/fleet/models/drivers.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Driver(models.Model):
     user = models.OneToOneField('users.User', on_delete=models.CASCADE, related_name='driver')
+    is_on_shift = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Driver: {self.user.username} (id: {self.pk})"
+        return f"{self.user.first_name} {self.user.last_name}"
